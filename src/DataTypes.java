26f68211ac8e469a7b96a0d3e9556843c37944be
Welcent Feng <welcent18@gmail.com>
2025-09-15 13:55:43 -0400

    task1.1

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -2,7 +2,7 @@ public class DataTypes {
     public static long sum(int[] numbers) {
         long s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
-        for (int x : numbers) {
+        for (int x : numbers) { //2
             s += x;
         }
         return s;
